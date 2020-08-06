f7e382223106d864f12d74741fbb36572006de8c
violettalivada <vilkalivada@gmail.com>
2020-08-06 12:30:01 +0300

    Модели зарегистрированы в админке

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -26,7 +26,7 @@ class Type(models.Model):
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=40, choices=STATUS_CHOICES, default='task', verbose_name='Статус')
+    name = models.CharField(max_length=40, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
 
     def __str__(self):
         return self.name
@@ -37,14 +37,14 @@ class Status(models.Model):
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False, default='Task', verbose_name='Заголовок')
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     type = models.ForeignKey('webapp.Type', related_name='type', on_delete=models.PROTECT, verbose_name='Тип')
     status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
 
     def __str__(self):
-        return "{}. {}".format(self.pk, self.description)
+        return "{}. {}".format(self.pk, self.title)
 
     class Meta:
         verbose_name = 'Задача'
