eeba3c431bdce8a961b25342c3adb8ad3aee73ba
violettalivada <vilkalivada@gmail.com>
2020-08-10 13:08:09 +0300

    Изменены модель и форма для поля "Тип" задачи и добавлена новая миграция

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from .models import *
 
 
-class TypeForm(forms.Form):
-    name = forms.ChoiceField(choices=TYPE_CHOICES, required=True, label='Тип', initial=DEFAULT_TYPE)
-
-
 class StatusForm(forms.Form):
     name = forms.ChoiceField(choices=STATUS_CHOICES, required=True, label='Тип', initial=DEFAULT_STATUS)
 
@@ -14,6 +10,7 @@ class TaskForm(forms.Form):
     title = forms.CharField(max_length=200, required=True, label='Заголовок')
     description = forms.CharField(max_length=3000, required=False, label='Описание', widget=forms.Textarea)
     date = forms.DateField(required=False, label='дата выполнения', widget=forms.DateInput(attrs={'type': 'date'}))
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='Тип', initial=DEFAULT_TYPE)
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Тип', initial=DEFAULT_TYPE,
+                                          widget=forms.SelectMultiple)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус', initial=DEFAULT_STATUS)
 
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+
 DEFAULT_TYPE = 'task'
 TYPE_CHOICES = [
     (DEFAULT_TYPE, 'Задача'),
     ('bug', 'Ошибка'),
     ('enhancement', 'Улучшение')
 ]
-
 
 DEFAULT_STATUS = 'new'
 STATUS_CHOICES = [
@@ -42,7 +42,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    type = models.ForeignKey('webapp.Type', related_name='type', on_delete=models.PROTECT, verbose_name='Тип')
+    type = models.ManyToManyField('webapp.Type', related_name='tasks', blank=True)
     status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
 
     def __str__(self):
