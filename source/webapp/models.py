8d722a013e73b2359dfecfd5ec219fd19b9853f4
violettalivada <vilkalivada@gmail.com>
2020-08-13 06:30:49 +0300

    В модель задачи добавлен базовый валидатор на проверку минимальной длины

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, RegexValidator
 
 
 DEFAULT_STATUS = 'new'
@@ -21,7 +22,8 @@ class Type(models.Model):
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=40, choices=STATUS_CHOICES, default=DEFAULT_STATUS, verbose_name='Статус')
+    name = models.CharField(max_length=40, choices=STATUS_CHOICES, default=DEFAULT_STATUS,
+                            verbose_name='Статус')
 
     def __str__(self):
         return self.name
@@ -32,11 +34,13 @@ class Status(models.Model):
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок',
+                             validators=(MinLengthValidator(10),))
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     types = models.ManyToManyField('webapp.Type', related_name='tasks', blank=True)
-    status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
+    status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT,
+                               verbose_name='Статус')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
