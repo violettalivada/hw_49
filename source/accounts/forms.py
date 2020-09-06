cad08373f427a00b99ec4413fa816533cb04cc9d
violettalivada <vilkalivada@gmail.com>
2020-09-07 00:59:58 +0300

    Добавлено редставление для регистрации

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'validate', }))
+
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,21 +1,26 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
 
 
-def login_view(request):
-    context = {}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            context['has_error'] = True
-    return render(request, 'registration/login.html', context=context)
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
 
-def logout_view(request):
-    logout(request)
-    return redirect('webapp:index')
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
